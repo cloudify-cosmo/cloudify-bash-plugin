e47c81ddd88d4ae3ac6f8d8ab35f487e13f9c2a8
opencm <limor@gigaspaces.com>
2014-10-27 12:34:28 +0200

    Bump version to 1.1rc1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-bash-plugin',
-    version='1.1a5',
+    version='1.1c1',
     author='rantav',
     author_email='rantav@gmail.com',
     packages=['bash_runner', 'bash_runner/resources'],
@@ -29,6 +29,6 @@ setup(
     license='LICENSE',
     description='Plugin for running simple bash scripts',
     install_requires=[
-        'cloudify-plugins-common==3.1a5',
+        'cloudify-plugins-common==3.1c1',
     ]
 )
